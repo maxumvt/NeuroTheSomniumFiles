a853704b35abb5d785da1c2c1d9077f77753fac5
Maxum <maxumvt@gmail.com>
2026-03-14 19:42:09 +0100

    Initial

---
+++ GameObserver.cs
@@ -0,0 +1,41 @@
+﻿namespace NeuroSomniumFiles;
+
+using BepInEx;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro; // if using TextMeshProUGUI
+
+
+[BepInPlugin("com.yourname.dialoglogger", "NeuroSomniumFiles", "1.0.0")]
+public class GameObserver : BaseUnityPlugin
+{
+    float searchTimer = 0f;
+    bool searchAllowed = true;
+    public 
+
+    void Awake()
+    {
+        Logger.LogInfo("NeuroSomniumFiles started");
+    }
+
+    void Update()
+    {
+        searchTimer += Time.deltaTime;
+        if ( searchTimer > 1f) { searchAllowed = true; searchTimer = 0f; }
+        else searchAllowed = false;
+
+        CharacterSpeaking(searchAllowed);
+        DescriptionText(searchAllowed);
+    }
+
+    // SECTION: Extractors
+    void CharacterSpeaking(bool allowSearch)
+    {
+        // 
+    }
+    void DescriptionText(bool allowSearch)
+    {
+        
+    }
+
+}
